6096ead05c671f1e57e82c900b6f7f19f6795d69
Nikhil Vipradas <117566735+nikhilvipradas@users.noreply.github.com>
2024-08-25 11:23:05 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3 + 3) //We are using nodejs and not browser
+
+//console.log(3+3) 
+//console.log("nikhil")
+
+//Code readability should be high
+
+let name = "nikhil"
+let age = "18"
+let isLoggedIn = "true"
+
+// number => 2 to power of 53
+// bigInt => 
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null);
