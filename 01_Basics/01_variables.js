af51111d0b1ae34f2f81e5c3c1ad5939fc2759ae
Nikhil Vipradas <117566735+nikhilvipradas@users.noreply.github.com>
2024-08-25 11:24:29 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "nikhil@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 //not allowed
+accountEmail = "vipradas@gmail.com"
+accountPassword = "212121"
+accountCity="Pune"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
