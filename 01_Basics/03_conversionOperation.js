e4b9b3bf14d131769b9b850575b4bef6dcd25c56
Nikhil Vipradas <117566735+nikhilvipradas@users.noreply.github.com>
2024-08-25 13:24:06 +0000

    conversion operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -28,3 +28,45 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// *************************OPERATIONS***************************
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = "Nikhil"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2")
+// console.log ("1" + 2 + 2)
+
+// console.log(1 + 2 + "2")
+
+// console.log((3+4) * 5 % 3)
+
+// console.log(true)
+// console.log(+true) //console.log(true+) => not allowed 
+// console.log(+"") //not preferred
+
+let gameCounter = 100
+
+gameCounter++;
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
+
+//link to study - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
